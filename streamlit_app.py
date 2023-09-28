62ca984ad32cdff9935c464ac7fef74adbcf0f80
goudaGanesh <119046278+goudaGanesh@users.noreply.github.com>
2023-09-28 13:58:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner');
 
-streamlit.header('Breakfast Menue');
-streamlit.text('Idli');
-streamlit.text('vada');
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
 
