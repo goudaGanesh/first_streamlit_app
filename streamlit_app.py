883e1fb89a44dea7d22bf43a8df9b4b144a9dce1
goudaGanesh <ganesh.gouda@mresult.com>
2023-09-29 12:16:53 +0530

    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,15 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('My Parents New Healthy Diner');
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 
 streamlit.header('Breakfast Menu')
 streamlit.text( '🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 🥑🍞Hard-Boiled Free-Range Egg')
 
-streamlit.text('through vs code')
-# another tses
+streamlit.dataframe(my_fruit_list);
 
 
