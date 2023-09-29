a05c6e3d7733c4f4a18067f29c40227f4269d95d
goudaGanesh <ganesh.gouda@mresult.com>
2023-09-29 12:22:51 +0530

    index

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 streamlit.title('My Parents New Healthy Diner');
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
-
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # streamlit.header('Breakfast Menu')
 # streamlit.text( '🥣Omega 3 & Blueberry Oatmeal')
 # streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
@@ -12,7 +12,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 
 # streamlit.dataframe(my_fruit_list);
 
-streamlit.multiselect('Pick Some Fruits:',list(my_fruit_list));
+streamlit.multiselect('Pick Some Fruits:',list(my_fruit_list.index));
 
 #dispslay table
 
