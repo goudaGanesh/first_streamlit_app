9bb890ba7b728769c5f6683a714e020ef99b4071
goudaGanesh <ganesh.gouda@mresult.com>
2023-09-29 12:20:02 +0530

    list select

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,15 @@ streamlit.title('My Parents New Healthy Diner');
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 
-streamlit.header('Breakfast Menu')
-streamlit.text( '🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 🥑🍞Hard-Boiled Free-Range Egg')
+# streamlit.header('Breakfast Menu')
+# streamlit.text( '🥣Omega 3 & Blueberry Oatmeal')
+# streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+# streamlit.text('🐔 🥑🍞Hard-Boiled Free-Range Egg')
+
+# streamlit.dataframe(my_fruit_list);
+
+streamlit.multiselect('Pick Some Fruits:',list(my_fruit_list));
+
+#dispslay table
 
 streamlit.dataframe(my_fruit_list);
-
-
