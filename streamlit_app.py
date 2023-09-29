0c91b474eb5f10311a4a1dcef4893d338183beb5
goudaGanesh <ganesh.gouda@mresult.com>
2023-09-29 12:35:36 +0530

    gg

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # streamlit.dataframe(my_fruit_list);
 
+streamlit.header('🥑🥑Build your own smoothie🥑🥑')
+
 fruits_selected = streamlit.multiselect('Pick Some Fruits:',list(my_fruit_list.index),['Avocado','Strawberries']);
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #dispslay n 
