02814eef221a6f88fcca4ab463788009c6a6d998
goudaGanesh <ganesh.gouda@mresult.com>
2023-09-29 11:59:52 +0530

    vs code

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,6 @@ streamlit.text( '🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 🥑🍞Hard-Boiled Free-Range Egg')
 
+streamlit.text('through vs code')
+
 
