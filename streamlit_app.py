61c37f046c32150966e714760360ea823cb6af85
goudaGanesh <ganesh.gouda@mresult.com>
2023-10-02 16:44:28 +0530

    zcv

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ except:
 # my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 # my_data_rows = my_cur.fetchall()
-streamlit.header("The Fruit Load List Contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute('SELECT * FROM FRUIT_LOAD_LIST')
@@ -61,7 +61,7 @@ def get_fruit_load_list():
 
 
 # add button
-if streamlit.button('View Our Fruit List - Add Your Favorites!'):
+if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
