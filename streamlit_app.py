7941575ad4d2659eb7c8bcc5033efe0d2c6810d2
goudaGanesh <ganesh.gouda@mresult.com>
2023-10-02 16:38:08 +0530

    asda

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,10 +67,18 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 
-streamlit.stop()
+# add fruit from user
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute(f"INSERT INTO FRUIT_LOAD_LIST VALUES('{new_fruit}')")
+        return f"Thanks for adding {new_fruit}"
 
-#new text box
-new_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write(f'Thanks for adding {new_fruit}');
 
-my_cur.execute(f"INSERT INTO FRUIT_LOAD_LIST VALUES('{new_fruit}')");
+new_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit To The List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
+    result = insert_row_snowflake(new_fruit)
+    streamlit.text(result)
+
+
+
