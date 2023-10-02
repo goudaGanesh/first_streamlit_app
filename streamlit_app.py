9ac6b326b48b75036b383690bbc2b9b272006413
goudaGanesh <ganesh.gouda@mresult.com>
2023-10-02 13:09:49 +0530

    askjdfhg

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ streamlit.dataframe(fruits_to_show);
 streamlit.header('Fruityvice Fruit Advice'); 
  # fruityvice api test
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 #normalising json response
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json());
