09e629cb77648fa2eaf718e76f72836f29bef2f8
goudaGanesh <ganesh.gouda@mresult.com>
2023-10-02 15:51:19 +0530

    sfc

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas as pd
 import requests
 import json
-
+import snowflake.connector
 streamlit.title('My Parents New Healthy Diner');
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
