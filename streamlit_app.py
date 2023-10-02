f9a649c9acb98486bb1ade0df40d483d2cbf2e43
goudaGanesh <ganesh.gouda@mresult.com>
2023-10-02 13:03:16 +0530

    api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.title('My Parents New Healthy Diner');
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# streamlit.header('Breakfast Menu')
-# streamlit.text( '🥣Omega 3 & Blueberry Oatmeal')
-# streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-# streamlit.text('🐔 🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text( '🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 🥑🍞Hard-Boiled Free-Range Egg')
 
 # streamlit.dataframe(my_fruit_list);
 
@@ -20,3 +21,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show);
  
+ # fruityvice api test
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
