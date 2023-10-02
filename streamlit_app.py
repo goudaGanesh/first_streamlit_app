4d256dbfc909a9c329192d4db44145f5a5ba8413
goudaGanesh <ganesh.gouda@mresult.com>
2023-10-02 16:13:32 +0530

    staap

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_cho
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json());
 streamlit.dataframe(fruityvice_normalized);
 
+streamlit.stop();
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
